c63c95c536c13f6dcadd396941f63e7f89b5e43b
Simon Brundin <simonbrundin@gmail.com>
2021-03-14 17:38:30 +0100

    migrerade till en nuxt app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential",
-    "plugin:prettier/recommended",
-    "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    browser: true,
+    node: true,
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
